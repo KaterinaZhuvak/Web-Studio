657473496d362e60edd306969a8b4725f631ce6c
KaterinaZhuvak <katerinazhuvak@icloud.com>
2023-08-08 15:06:10 +0100

    new commit

---
+++ JS/modal.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const openModalBtn = document.querySelector(".open_modal");
+  const closeModalBtn = document.querySelector(".close_modal");
+  const modal = document.querySelector(".overlay");
+
+  openModalBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+  });
+  closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+  });
+});
